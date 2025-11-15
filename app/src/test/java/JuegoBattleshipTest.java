85cce531e39c32797ad4338bd84cbb3caeb4be74
Daniel Antonio Osuna Osuna <dosuna19@uabc.edu.mx>
2025-11-14 18:54:06 -0800

    Programa Original

---
+++ app/src/test/java/JuegoBattleshipTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+class JuegoBattleshipTest {
+
+    @Test
+    void recibirDisparo() {
+    }
+
+    @Test
+    void yaDisparado() {
+    }
+
+    @Test
+    void obtenerTipoBarcoEn() {
+    }
+
+    @Test
+    void estaBarcoHundido() {
+    }
+
+    @Test
+    void todosBarcosHundidos() {
+    }
+}
---
+++ app/src/test/java/ProtocoloBattleshipTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+class ProtocoloBattleshipTest {
+
+    @Test
+    void construirMensajeDisparo() {
+
+    }
+
+    @Test
+    void construirMensajeResultado() {
+
+    }
+
+    @Test
+    void parsearMensaje() {
+
+    }
+}
